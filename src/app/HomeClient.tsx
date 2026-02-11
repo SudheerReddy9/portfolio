617810d32bf9e42e563b5c14139c1f955e95b180
Sudheer Reddy <pokalasudheer143@gmail.com>
2026-02-11 09:10:00 -0500

    Home page is updated && added details

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -1,26 +1,74 @@
-import { Box, Typography } from "@mui/material"
-
+import { Box, Button, Typography } from "@mui/material"
+import Image from "next/image";
 const ClinetHome: React.FC = () => {
     return (
         <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                p: 10
 
+            }}
         >
             <Box
                 sx={{
-                    justifyContent: 'center',
-                    alignContent: 'center',
-                    alignItems: 'center',
-                    justifyItems: 'center',
-                    textAlign: 'center',
+                    maxWidth: 600
                 }}
             >
-                <Typography variant="h5"
+                <Typography variant="h2"
                     sx={{
-                        p: '10px',
+                        fontWeight: 700,
+                        mb: 2
                     }}
-                >Welcome</Typography>
-                <Typography variant="h5">Hello, This is <strong> Venkata Sudheer</strong></Typography>
+                >Frontend & Web Developer</Typography>
+                <Typography variant='h5'
+                    sx={{
+                        color: '#64748B',
+                        mb: 4,
+                    }}
+                >
+                    I build scalable, high-performance web applications
+                    using React, Next.js, and TypeScript.
+                </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        gap: 2
+                    }}
+                >
+                    <Button variant="contained"
+                        sx={{
 
+                        }}
+                    >View Projects</Button>
+                    <Button variant="contained"
+                        sx={{
+
+                        }}
+                    >Download Resume</Button>
+                </Box>
+
+            </Box>
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    gap: 2,
+                    borderRadius: '16px',
+                    backgroundColor: 'black',
+                    position: 'relative',
+                    width: 300,
+                    height: 200,
+                }}
+            >
+                <Image
+                    src="/Aro_Logo-White.png"
+                    alt="Sudheer"
+                    fill
+                    style={{ objectFit: 'contain' }}
+                />
             </Box>
         </Box>
     )
